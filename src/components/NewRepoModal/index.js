066e375effd74aa22e61cd70805222d58f410f2d
Ramon Wiliam <roniewill@gmail.com>
2018-05-31 13:00:09 -0300

    updated styles file

--- src/components/NewRepoModal/index.js
+++ src/components/NewRepoModal/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 
+import styles from './styles';
+
 class NewRepoModal extends Component {
     render () {
         return (
@@ -35,58 +37,5 @@ class NewRepoModal extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    modalContainer: {
-        flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    boxContainer: {
-        padding: 20,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        alignItems: 'center',
-        width: 300,
-    },
-    boxTitle: {
-        fontSize: 16,
-        fontWeight: 'bold'
-    },
-    boxInput: {
-        alignSelf: 'stretch',
-        marginTop: 10,
-        paddingVertical: 0,
-        paddingHorizontal: 20,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        height: 40,
-        borderRadius: 3
-    },
-    buttomContainer: {
-        flexDirection: 'row',
-        marginTop: 10,
-        height: 40
-    },
-    buttom: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 3
-    },
-    submitButtom: {
-        backgroundColor: '#00b300',
-        marginLeft: 5
-    },
-    cancelButtom: {
-        backgroundColor: '#ff4d4d',
-        marginRight: 5
-    },
-    buttomText: {
-        color: '#fff',
-        fontWeight: 'bold'
-    }
-});
 
 export default NewRepoModal;
---
+++ src/components/NewRepoModal/styles.js
@@ -0,0 +1,52 @@
+export default {
+    modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    boxContainer: {
+        padding: 20,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        alignItems: 'center',
+        width: 300,
+    },
+    boxTitle: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    boxInput: {
+        alignSelf: 'stretch',
+        marginTop: 10,
+        paddingVertical: 0,
+        paddingHorizontal: 20,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        height: 40,
+        borderRadius: 3
+    },
+    buttomContainer: {
+        flexDirection: 'row',
+        marginTop: 10,
+        height: 40
+    },
+    buttom: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 3
+    },
+    submitButtom: {
+        backgroundColor: '#00b300',
+        marginLeft: 5
+    },
+    cancelButtom: {
+        backgroundColor: '#ff4d4d',
+        marginRight: 5
+    },
+    buttomText: {
+        color: '#fff',
+        fontWeight: 'bold'
+    }
+}
